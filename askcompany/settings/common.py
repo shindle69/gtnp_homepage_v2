f6205741b24b4c50f15ec63b524d33d62680f688
APPLE <apple@APPLEui-iMac.local>
2018-12-11 15:03:57 +0900

    debug=false

--- askcompany/settings/common.py
+++ askcompany/settings/common.py
@@ -24,7 +24,7 @@ BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
 SECRET_KEY = 'sinbee_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  True
+DEBUG =  False
 
 ALLOWED_HOSTS = ['127.0.0.1','*.heroku.com']
 
